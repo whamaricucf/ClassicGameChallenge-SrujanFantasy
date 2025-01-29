51a525f14ea5a7d4676accf20ec4c6ee8189cea8
Hamaric <angryearl@gmail.com>
2025-01-29 14:25:33 -0500

    Create Combat.cs
    unfinished combat code
    this has half of the damage calculator which calculates whether a physical attack hits and how much damage it does based on the attacker's and defender's stats.

---
+++ Code Branch/Combat.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Combat : MonoBehaviour
+{
+    private int atkDmg;
+    private int damageRating;
+
+    private int str;
+    private int mag;
+    private int def;
+    private int HP;
+    private int agi;
+    private int lvl;
+
+    private int targetStr;
+    private int targetMag;
+    private int targetDef;
+    private int targetHP;
+    private int targetAgi;
+    private int targetLvl;
+
+    private int turn;
+
+    public bool magAtk;
+    public bool backRow;
+    public bool targetBackRow;
+    public bool targetStunned;
+
+    static Random rnd = new Random();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("");
+        DamageCalculator(10, 0, 8, 1, 4, 0, 5, 5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int DamageCalculator(int strValue, int magValue, int agiValue, int lvlValue, int targetValue, int targetMagValue, int targetDefValue, int targetAgiValue, int targetLvlValue)
+    {
+        int hitCritChance = rnd.Next(0, 200) + 1;
+        hitCritChance = 1;
+        if (!magAtk)
+        {
+            DamageRating();
+            PhysAttackChance();
+        }
+        else
+        {
+
+        }
+
+        void PhysAttackChance()
+        {
+            if (hitCritChance != 200 && hitCritChance <= agiValue * 2 && hitCritChance <= 100 - targetAgiValue / 2 || hitCritChance == 1)
+            {
+                Debug.Log("Crit!");
+                int damage = rnd.Next(damageRating, (damageRating + 1) * 2);
+                Debug.Log("Pre-Crit Damage: " + damage);
+                atkDmg = damage * 2;
+            }
+            else if (hitCritChance != 200 && hitCritChance <= 150 + (agiValue / 2) - (targetAgiValue / 4) || targetStunned == true)
+            {
+                Debug.Log("Hit!");
+                Debug.Log("# Needed to Crit: " + (agiValue * 2));
+                PhysicalAttack();
+            }
+            else
+            {
+                Debug.Log("Missed!");
+                Debug.Log("# Needed to Hit: " + (150 + (agiValue / 2) - (targetAgiValue / 4)));
+                atkDmg = 0;
+            }
+            Debug.Log("Hit Chance: " + hitCritChance);
+
+        }
+
+        void DamageRating()
+        {
+            if (!backRow)
+            {
+                damageRating = ((strValue / 2) * ((lvlValue + 1)/ 2));
+            }
+            else
+            {
+                damageRating = ((strValue / 4) * ((lvlValue + 1) / 2));
+            }
+            Debug.Log("Damage Rating: " + damageRating);
+        }
+
+        void PhysicalAttack()
+        {
+            int damage = rnd.Next(damageRating, damageRating * 2);
+            atkDmg = damage - targetDefValue;
+            Debug.Log("Raw Damage: " + damage);
+        }
+
+        if (atkDmg < 0)
+        {
+            if (targetValue <= 3)
+            {
+                atkDmg = 0;
+            } else if (targetValue > 3)
+            {
+                atkDmg = 1;
+            }
+        }
+        else if (atkDmg > 9999)
+        {
+            atkDmg = 9999;
+        }
+
+        Debug.Log("Damage: " + atkDmg);
+        return atkDmg;
+    }
+}
